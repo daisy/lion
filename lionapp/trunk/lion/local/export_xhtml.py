2f2447eddd97d7b11aa456791cefa7c0386da326
Marisa DeMeglio <marisa.demeglio@gmail.com>
2008-04-07 10:15:16 +0000

    wrote export to xhtml

--- lionapp/trunk/lion/local/export_xhtml.py
+++ lionapp/trunk/lion/local/export_xhtml.py
@@ -1,3 +1,20 @@
+from managedb import DBSession
+
+#note that this is also defined for the web scripts
+#we could share when the managedb stuff goes online instead of locally
+XHTML_TEMPLATE = """Content-type: text/html; charset=utf-8
+
+<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>%(TITLE)s</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link href="http://amisproject.org/translate/l10n.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>%(BODY)s</body>
+</html>"""
 
 def export_xhtml(session, langid):
     """export xhtml from the database
@@ -9,10 +26,17 @@ def export_xhtml(session, langid):
        <h1 id="t2">F</h1>
        <h1 id="t3">Alt + F</h1>
        ...
-       
-       """
-       session.execute_query
-       
+    """
+    thislang = session.check_language(langid)
+    body = """<h1 id="thislang">%s</h1>""" % thislang
+    table = langid.replace("-", "_")
+    request = "SELECT xmlid, textstring from %s" % table
+    session.execute_query(request)
+    for id, txt in session.cursor:
+        body += """<h1 id="%s">%s</h1>""" % (id, txt)
+    print XHTML_TEMPLATE % (thislang, body)    
+
+           
     
     
     
